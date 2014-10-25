495031cf9e3627696c4b5b7cacf9bc5301d7d6a7
smartshinobu <bizan07@bizankomumakku07-no-MacBook-Air-2.local>
2014-10-25 22:54:02 +0900

    Initial Commit

---
+++ alertcontroller/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  alertcontroller
+//
+//  Created by ビザンコムマック０７ on 2014/10/25.
+//  Copyright (c) 2014年 mycompany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ alertcontroller/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  alertcontroller
+//
+//  Created by ビザンコムマック０７ on 2014/10/25.
+//  Copyright (c) 2014年 mycompany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
